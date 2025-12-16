972ddfdf3dbc244b3462ddead3a805644486f259
pallavi188 <officialpallavi414@gmail.com>
2025-12-16 15:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.76%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int idx= 0;
+        for(int i=0;i<word.size();i++){
+            if(word[i]==ch) {
+                idx = i;
+                break;
+            }
+
+        }
+       reverse(word.begin(),word.begin()+idx+1);
+       return word;
+
+    }
+};
