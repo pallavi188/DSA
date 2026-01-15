45bbe2317c9163e9a8ef2b689cd7b420e0016678
pallavi188 <officialpallavi414@gmail.com>
2026-01-15 22:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (58.67%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        map<char,int>mp;
+        for(char ch : jewels){
+            mp[ch]++;
+        }
+        int cnt = 0;
+        for(int i=0;i<stones.length();i++){
+            if(mp.find(stones[i]) != mp.end()) cnt++;
+        }
+        return cnt;
+    }
+};
