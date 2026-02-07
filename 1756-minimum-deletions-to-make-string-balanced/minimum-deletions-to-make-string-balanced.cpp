0a6688f770ce6ff3b7f09c8753cb047341457678
pallavi188 <officialpallavi414@gmail.com>
2026-02-07 12:17:33 +0530

    Time: 17 ms (76.78%) | Memory: 25.4 MB (87.57%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bCount = 0;
+        int del = 0;
+        for(char c: s){
+            if(c=='b'){
+                bCount++;
+            }else{
+                del = min(del+1,bCount);
+            }
+        }
+        return del;
+    }
+};
