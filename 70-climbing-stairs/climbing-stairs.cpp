5dbf2f12ae874deb0d3fe2b5080d12e7e1dcfe41
pallavi188 <officialpallavi414@gmail.com>
2026-05-25 19:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int f(int n,vector<int>&dp){
-        if(n==0 || n==1) return 1;
-        if(dp[n] != -1) return dp[n];
-        int pick = f(n-1,dp);
-        int not_pick = f(n-2,dp);
-
-        return dp[n]= pick + not_pick;
+    int f(int idx,vector<int>&dp){
+        if(idx==0 || idx==1 || idx<2)return 1;
+        if(dp[idx] != -1) return dp[idx];
+        int pick1 = f(idx-1,dp);
+        int pick2 = f(idx-2,dp);
+        return dp[idx]=pick1+pick2;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
