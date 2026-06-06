da14918df41e11f9d18501954727f87cc65c8ed4
pallavi188 <officialpallavi414@gmail.com>
2026-06-06 18:13:50 +0530

    Time: 19 ms (10.24%) | Memory: 19.8 MB (5.58%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    int f(int st,int et,vector<int>&piles,vector<vector<int>>&dp){
+        if(st>et) return 0;
+        if(dp[st][et] != -1) return dp[st][et];
+        return dp[st][et] = max(piles[st]-f(st+1,et,piles,dp),piles[et]-f(st,et-1,piles,dp));
+
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return f(0,n-1,piles,dp)>0;
     }
 };
