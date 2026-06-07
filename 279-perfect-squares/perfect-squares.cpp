f97270f70a04190b5100dbd6d30b6fce5f37d80f
pallavi188 <officialpallavi414@gmail.com>
2026-06-07 13:00:40 +0530

    Time: 220 ms (11.91%) | Memory: 160.2 MB (10.39%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int f(int idx,int n,vector<int>&arr,vector<vector<int>>&dp){
-        if(idx == 0){
-           if(n%arr[0] == 0)return n/arr[0];
-           else
-           return 1e9; 
-        }
-        if(dp[idx][n] != -1) return dp[idx][n];
-        int not_pick = f(idx-1,n,arr,dp);
-        int pick = 1e9;
-        if(arr[idx]<=n)
-        pick = 1+f(idx,n-arr[idx],arr,dp);
-        return dp[idx][n] = min(pick,not_pick);
-    }
+  
     int numSquares(int n) {
         vector<int>arr;
         for(int i=1;i*i<=n;i++){
             arr.push_back(i*i);
         }
         int num = arr.size();
-        vector<vector<int>>dp(num,vector<int>(n+1,-1));
-        return f(num-1,n,arr,dp);
+        vector<vector<int>>dp(num,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++){
+            if(i%arr[0]==0)dp[0][i] = i/arr[0];
+            else
+            dp[0][i] = 1e9;
+        }
+        for(int idx=1;idx<num;idx++){
+            for(int i=0;i<=n;i++){
+                int not_pick = dp[idx-1][i];
+                int pick = 1e9;
+                if(arr[idx]<=i)
+                pick = 1 + dp[idx][i-arr[idx]];
+
+                dp[idx][i] = min(pick,not_pick);
+            }
+        }
+        return dp[num-1][n];
     }
 };
