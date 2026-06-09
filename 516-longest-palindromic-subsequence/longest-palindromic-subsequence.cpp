1062508f502cf01836cdfd6555357d4381759824
pallavi188 <officialpallavi414@gmail.com>
2026-06-09 10:27:58 +0530

    Time: 100 ms (8.28%) | Memory: 76.1 MB (21.09%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-int lcs(string text1, string text2, vector<vector<int>>& dp) {
-        int n = text1.size();
-        int m = text2.size();
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    int LCS(string s,string t,vector<vector<int>>&dp){
+        int n = s.length();
+        for(int i=0;i<=n;i++)dp[0][i]=0;
+        for(int j=0;j<=n;j++) dp[j][0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return dp[n][m];
-}
+        return dp[n][n];
+    }
     int longestPalindromeSubseq(string s) {
-         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-         string t = s;
-        reverse(t.begin(), t.end());
-        return lcs(s, t, dp);
+        int n = s.length();
+        string t = s;
+        reverse(t.begin(),t.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        return LCS(s,t,dp);
     }
 };
