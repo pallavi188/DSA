78e70ecb8dbe8ce39ca56c8bdd94f2691fe62941
pallavi188 <officialpallavi414@gmail.com>
2026-06-10 10:52:41 +0530

    Time: 27 ms (38.14%) | Memory: 26.6 MB (94.40%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size(),m=str2.size();
+        int n = str1.length();
+        int m = str2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int j=0;j<=m;j++)dp[0][j] = 0;
+        for(int i=0;i<=n;i++) dp[i][0] =0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                if(str1[i-1]==str2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        string ans = "";
-        int i = n,j = m;
+        //to print 
+        string ans ="";
+        int i =n,j=m;
         while(i>0 && j>0){
-            if(str1[i-1]==str2[j-1]) {
+            if(str1[i-1] == str2[j-1]){
                 ans += str1[i-1];
-                i--,j--;
-            }else if(dp[i-1][j] > dp[i][j-1]){
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
                 ans += str1[i-1];
                 i--;
             }else{
@@ -27,7 +30,7 @@ public:
                 j--;
             }
         }
-        while(i>0){
+        while(i >0){ 
             ans += str1[i-1];
             i--;
         }
