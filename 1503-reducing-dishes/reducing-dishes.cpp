0172ea2475ad96268dc5ea0f8dfa700e746cfc8f
pallavi188 <officialpallavi414@gmail.com>
2026-06-15 10:23:40 +0530

    Time: 20 ms (28.61%) | Memory: 25 MB (27.19%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int f(int idx,int time,vector<int>&sat,vector<vector<int>>&dp){
-        int n = sat.size();
-        if(idx == n)return 0;
-        if(dp[idx][time] != -1) return dp[idx][time];
-        int take = time*sat[idx] + f(idx+1,time+1,sat,dp);
-        int not_take = f(idx+1,time,sat,dp);
-        return dp[idx][time] = max(take,not_take);
-    }
+    
     int maxSatisfaction(vector<int>& sat) {
         int n = sat.size();
         sort(sat.begin(),sat.end());
