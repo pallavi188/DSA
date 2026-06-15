f36e4dad874cb967a445450cb5671d0e66c72616
pallavi188 <officialpallavi414@gmail.com>
2026-06-15 10:23:22 +0530

    Time: 19 ms (30.89%) | Memory: 25 MB (27.19%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -11,7 +11,14 @@ public:
     int maxSatisfaction(vector<int>& sat) {
         int n = sat.size();
         sort(sat.begin(),sat.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return f(0,1,sat,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+2,0));
+        for(int idx=n-1;idx>=0;idx--){
+            for(int time=1;time<=n;time++){
+                int take = time*sat[idx] + dp[idx+1][time+1];
+                int not_take = dp[idx+1][time];
+                dp[idx][time] = max(take,not_take);
+            }
+        }
+        return dp[0][1];
     }
 };
