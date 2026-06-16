d3f2f4cab5a815a5b741f9130c33226446e6420a
pallavi188 <officialpallavi414@gmail.com>
2026-06-16 11:12:00 +0530

    Time: 848 ms (8.60%) | Memory: 9.4 MB (80.58%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
-        if (s.length() <= 1) {
-            return s;
-        }
+     bool f(int i,int j,string &s){
+        if(i>=j) return true;
+        if(s[i] == s[j]) return f(i+1,j-1,s);
         
-        int max_len = 1;
-        int start = 0;
-        int end = 0;
-        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
-        
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > max_len) {
-                        max_len = i - j + 1;
-                        start = j;
-                        end = i;
+        return false;
+     }
+     string longestPalindrome(string s){
+        int n = s.length();
+        int maxLen =0;
+        int startPoint =0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(f(i,j,s) == true){
+                    if(j-i+1 > maxLen){
+                        maxLen = j-i+1;
+                        startPoint = i;
                     }
                 }
             }
         }
-        
-        return s.substr(start, end - start + 1);
-    }
+        return s.substr(startPoint,maxLen);
+     }
 };
