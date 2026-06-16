1b3cf326784972c5a807586edd2333cc9b14c905
pallavi188 <officialpallavi414@gmail.com>
2026-06-16 12:44:15 +0530

    Time: 87 ms (44.77%) | Memory: 84.6 MB (12.65%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int f(int i,int j,vector<string>& words, string& target,
-    vector<vector<long long>>& freq,vector<vector<long long>>& dp){
-         int n = target.length();
-        int k = words[0].size();
-        if(i == n) return 1;
-        if(j == k) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        long long not_taken = f(i,j+1,words,target,freq,dp)%mod;
-        long long taken = (freq[target[i]-'a'][j] *
-        f(i+1,j+1,words,target,freq,dp))%mod;
-        return dp[i][j]= (taken + not_taken)%mod;
-    }
+   
     int numWays(vector<string>& words, string target) {
         int n = target.length();
         int k = words[0].size();
