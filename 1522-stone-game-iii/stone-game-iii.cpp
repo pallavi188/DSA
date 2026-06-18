cc1148b08d5d17ac5b1a6f4da2993c926163620c
pallavi188 <officialpallavi414@gmail.com>
2026-06-18 11:16:32 +0530

    Time: 11 ms (87.08%) | Memory: 136.1 MB (71.75%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int f(int idx,vector<int>&sv,vector<int>&dp){
-       int n = sv.size();
-        if(idx>=n) return 0;
-        int result = -1e9;
-        if(dp[idx] != -1) return dp[idx];
-        result = max(result,sv[idx] - f(idx+1,sv,dp));
-        if(idx+1<n)
-        result = max(result,sv[idx]+sv[idx+1]-f(idx+2,sv,dp));
-        if(idx+2<n)
-        result = max(result,sv[idx]+sv[idx+1]+sv[idx+2]-f(idx+3,sv,dp));
-        return dp[idx]= result;
-    }
     string stoneGameIII(vector<int>& sv) {
         int n = sv.size();
-        vector<int>dp(n+1,-1);
-        int diff = f(0,sv,dp);
-        if(diff>0) return "Alice";
-        else if(diff<0) return "Bob";
+        vector<int>dp(n+1,0);
+
+        for(int idx=n-1;idx>=0;idx--){
+            int res = -1e9;
+            res = max(res,sv[idx] - dp[idx+1]);
+            if(idx+1<n) res = max(res,sv[idx]+sv[idx+1]-dp[idx+2]);
+            if(idx+2<n) res = max(res,sv[idx]+sv[idx+1]+sv[idx+2]-dp[idx+3]);
+            dp[idx] = res;
+        }         
+        if(dp[0]>0) return "Alice";
+        else if(dp[0]<0) return "Bob";
         else
         return "Tie";
     }
