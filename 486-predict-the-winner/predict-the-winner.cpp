a6c122d07cb9f3f592f8bd7a74f7960ddc206133
pallavi188 <officialpallavi414@gmail.com>
2026-06-21 16:48:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (43.03%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>& nums){
+    int f(int i,int j,vector<int>& nums,vector<vector<int>>&dp){
         int n = nums.size();
         if(i > j) return 0;
         if(i == j) return nums[i];
-        int take_i = nums[i] + min(f(i+2,j,nums),f(i+1,j-1,nums));
-        int take_j = nums[j] + min(f(i+1,j-1,nums),f(i,j-2,nums));
-        return max(take_i,take_j);
+        if(dp[i][j] != -1) return dp[i][j];
+        int take_i = nums[i] + min(f(i+2,j,nums,dp),f(i+1,j-1,nums,dp));
+        int take_j = nums[j] + min(f(i+1,j-1,nums,dp),f(i,j-2,nums,dp));
+        return dp[i][j] = max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int score1 = f(0,n-1,nums);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int score1 = f(0,n-1,nums,dp);
         int sum =0;
         for(int i=0;i<n;i++){
             sum += nums[i];
