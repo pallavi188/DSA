b2ab2884e3157344d9cb430b4aa4ce1bf38abe9b
pallavi188 <officialpallavi414@gmail.com>
2026-06-23 18:22:15 +0530

    Time: 4 ms (7.71%) | Memory: 13.2 MB (52.89%) - LeetSync

---
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        int k = n * 5/100;
+        double sum =0;
+        for(int i=k;i<n-k;i++){
+            sum += arr[i];
+        }
+        return sum/(n-2*k);
+    }
+};
