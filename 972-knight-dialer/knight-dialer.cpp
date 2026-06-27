8105160506a1a03bea563c7c9390c6bc99f3851e
pallavi188 <officialpallavi414@gmail.com>
2026-06-27 10:12:47 +0530

    Time: 91 ms (52.52%) | Memory: 40.5 MB (44.90%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -14,21 +14,24 @@ public:
          {1,3},
          {2,4}
     };
-    int f(int n,int cell,vector<vector<int>>&dp){
-        if(n == 0) return 1;
-        int res = 0;
-        if(dp[n][cell] != -1) return dp[n][cell];
-        for(int &nextCell : adj[cell]){
-            res = (res + f(n-1,nextCell,dp))%mod;
-        }
-        return dp[n][cell] = res;
-    }
+   
     int knightDialer(int n) {
-        int ans = 0;
-        vector<vector<int>>dp(n+1,vector<int>(10,-1));
-        for(int cell=0;cell<=9;cell++){
-            ans = (ans + f(n-1,cell,dp))%mod;
+        
+        vector<vector<int>>dp(n+1,vector<int>(10,0));
+        for(int cell=0;cell<=9;cell++) dp[0][cell] = 1;
+        for(int N = 1;N<n;N++){
+            for(int cell=0;cell<=9;cell++){
+                int ans = 0;
+                for(int &nextCell : adj[cell]){
+                    ans = (ans + dp[N-1][nextCell])%mod;
+                }
+                dp[N][cell] = ans;
         }
-        return ans;
+        }
+        int res = 0;
+        for(int cell=0;cell<=9;cell++){
+            res = (res + dp[n-1][cell])%mod;
+        }
+        return res;
     }
 };
