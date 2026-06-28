2ffe3f3825f3da8e3d3e1938a7213cc61e0a2ac4
pallavi188 <officialpallavi414@gmail.com>
2026-06-28 11:16:31 +0530

    Time: 64 ms (63.57%) | Memory: 69.6 MB (86.28%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    
-    int f(int idx,string s,unordered_set<string>&st,vector<int>&dp){
-        int n = s.length();
-        if(idx >= n) return 0;
-        int minExtra =n;
-        string currStr = "";
-        if(dp[idx] != -1) return dp[idx];
-        for(int i=idx;i<n;i++){
-            currStr.push_back(s[i]);
-            int currExtra = (st.find(currStr) == st.end()) ? currStr.length():0;
-            int remainingExtra =f(i + 1,s,st,dp);
-            minExtra = min(minExtra,currExtra + remainingExtra);
-        }
-        return dp[idx] = minExtra;
-    }
     int minExtraChar(string s, vector<string>& dictionary) {
        unordered_set<string>st;
        int n = s.length();
-       vector<int>dp(n+1,-1);
+       vector<int>dp(n+1,0);
        for(string &word : dictionary){
         st.insert(word);
        }
-        return f(0,s,st,dp);
+     
+       
+        for(int idx = n-1;idx>=0;idx--){
+            string currStr = "";
+            int minExtra = 1+dp[idx+1];
+            for(int i=idx;i<n;i++){
+            currStr.push_back(s[i]);
+            int currExtra = (st.find(currStr) == st.end()) ? currStr.length():0;
+            int remainingExtra =dp[i+1];
+            minExtra = min(minExtra,currExtra + remainingExtra);
+
+        }
+        dp[idx] = minExtra;
+        }
+        return dp[0];
     }
 };
