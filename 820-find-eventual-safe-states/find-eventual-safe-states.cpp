35968919587e71da42e9d99f391e8b790723b95c
pallavi188 <officialpallavi414@gmail.com>
2026-07-01 18:04:35 +0530

    Time: 7 ms (80.21%) | Memory: 53.2 MB (61.57%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
+    bool dfsCheck(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&pathVis,
+    vector<int>&check){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(auto it : graph[node]){
+            if(!vis[it]){
+                if(dfsCheck(it,graph,vis,pathVis,check)) {
+                    
+                    return true;
+                }
+            }else if(pathVis[it]){
+               
+                return true;
+            }
+        }
+        pathVis[node] = 0;
+        check[node] = 1;
+        return false;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-     
-        //reverse the adj list
-       vector<vector<int>>adjRev(V);
-        vector<int>indegree(V,0);
-        for(int i=0;i<V;i++){
-            for(auto it : graph[i]){
-                adjRev[it].push_back(i);
-                indegree[i]++;
-            }
-        }
-        queue<int>q;
+        int v = graph.size();
+       vector<int>vis(v,0);
+        vector<int>pathVis(v,0);
+        vector<int>check(v,0);
         vector<int>safeNode;
-        for(int i=0;i<V;i++){
-            if(indegree[i] == 0)q.push(i);
-        }
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            safeNode.push_back(node);
-            for(auto it : adjRev[node]){
-                indegree[it]--;
-                if(indegree[it]==0)q.push(it);
+        for(int i=0;i<v;i++){
+            if(!vis[i]){
+                dfsCheck(i,graph,vis,pathVis,check);
             }
         }
-        sort(safeNode.begin(),safeNode.end());
+        for(int i=0;i<v;i++){
+            if(check[i]==1)safeNode.push_back(i);
+        }
         return safeNode;
     }
 };
