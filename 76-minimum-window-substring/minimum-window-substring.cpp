625696dd2ba027ec62cfd71ec249da7700286527
pallavi188 <officialpallavi414@gmail.com>
2026-07-11 13:52:52 +0530

    Time: 11 ms (47.36%) | Memory: 11.4 MB (65.53%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+        if(t.length()>n)return "";
+        unordered_map<char,int>mp;
+        for(char &ch : t)mp[ch]++;
+        int reqCount = t.length();
+        int i =0,j=0;
+        int minWindowSize = INT_MAX;
+        int start_i = 0;
+        while(j<n){
+            char ch = s[j];
+            if(mp[ch] > 0)reqCount--;
+            mp[ch]--;
+
+            while(reqCount == 0){
+                int currWindowSize = j-i+1;
+                if(minWindowSize > currWindowSize){
+                    minWindowSize = currWindowSize;
+                    start_i = i;
+                }
+                mp[s[i]]++;
+                if(mp[s[i]] > 0) reqCount++;
+                i++;
+            }
+            j++;
+        }
+       return minWindowSize == INT_MAX ? "" : s.substr(start_i,minWindowSize); 
+    }
+};
