66153800c288c27f1bb531c1251f6a869181148a
pallavi188 <officialpallavi414@gmail.com>
2026-07-11 14:37:02 +0530

    Time: 43 ms (22.21%) | Memory: 24.2 MB (12.65%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
-        if(n == 1) return 1;
-        int result = 0;
+        if(n==1)return 1;
+        int result =0;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int cnt =2;
-                int dx = points[j][0]-points[i][0];
-                int dy = points[j][1]-points[i][1];
-               
-                for(int k=0;k<n;k++){
-                    if(k != i && k!=j){
-                        int dx_ = points[k][0] - points[i][0];
-                        int dy_ = points[k][1] - points[i][1];
-
-                        if(dy*dx_ == dx*dy_){
-                            cnt++;
-                        }
-                    }
-                }
-                result = max(result,cnt);
+            unordered_map<double,int>mp;
+            for(int j=0;j<n;j++){
+                if(i == j) continue;
+                auto dy = points[j][1]-points[i][1];
+                auto dx = points[j][0]-points[i][0];
+                 double theta = atan2(dy,dx);
+                 mp[theta]++;
+            }
+            for(auto &it:mp){
+                result = max(result,it.second + 1);
             }
         }
         return result;
