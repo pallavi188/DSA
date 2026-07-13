708500829e72386928b66045026947a3c1163953
pallavi188 <officialpallavi414@gmail.com>
2026-07-13 17:44:00 +0530

    Time: 23 ms (84.20%) | Memory: 17.6 MB (43.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     vector<vector<int>>directions{{1,0},{-1,0},{0,-1},{0,1}};
     int n,m;
-    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&grid){
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&grid){
         vis[row][col] = 1;
-        queue<pair<int,int>>q;
-        q.push({row,col});
-        while(!q.empty()){
-            int curr_row = q.front().first;
-            int curr_col = q.front().second;
-            q.pop();
-            //traverse all its neighbours and push them in the queue if not visited
-            for(vector<int>&dir : directions){
-                int new_row = curr_row + dir[0];
-                int new_col = curr_col + dir[1];
-                if(new_row>=0 && new_row < n && new_col>=0 && new_col < m && !vis[new_row][new_col]&&grid[new_row][new_col]=='1'){
-                    vis[new_row][new_col] = 1;
-                    q.push({new_row,new_col});
-                }
+        for(vector<int>&dir : directions){
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if(newRow >=0 && newRow <n && newCol>=0 && newCol <m && !vis[newRow][newCol] && grid[newRow][newCol]=='1'){
+                dfs(newRow,newCol,vis,grid);
             }
         }
     }
@@ -30,7 +21,7 @@ public:
             for(int col =0;col<m;col++){
                 if(!vis[row][col] && grid[row][col]=='1'){
                     cnt++;
-                    bfs(row,col,vis,grid);
+                    dfs(row,col,vis,grid);
                 }
             }
         }
