769a5c930bb4da0fee36274b1224db8b5146f1fb
pallavi188 <officialpallavi414@gmail.com>
2026-07-30 00:20:36 +0530

    Time: 2 ms (64.67%) | Memory: 9.7 MB (40.25%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
 int n,m,N;
-    bool f(int i,int j,int k, string &s1,string &s2,string &s3,vector<vector<vector<int>>>&dp){
-        if(i==n && j==m && k==N)return true;
-        if(k>=N)return false;
+    bool f(int i,int j, string &s1,string &s2,string &s3,vector<vector<int>>&dp){
+        if(i==n && j==m && i+j==N)return true;
+        if(i+j>=N)return false;
         bool result = false;
-        if(dp[i][j][k] != -1)return  dp[i][j][k];
-        if(s1[i]==s3[k]){
-            result = f(i+1,j,k+1,s1,s2,s3,dp);
+        if(dp[i][j] != -1)return  dp[i][j];
+        if(s1[i]==s3[i+j]){
+            result = f(i+1,j,s1,s2,s3,dp);
         }
-        if(result == true) return dp[i][j][k] = true;
-        if(s2[j] == s3[k]){
-            result = f(i,j+1,k+1,s1,s2,s3,dp);
+        if(result == true) return dp[i][j] = true;
+        if(s2[j] == s3[i+j]){
+            result = f(i,j+1,s1,s2,s3,dp);
         }
-        return dp[i][j][k] =  result;
+        return dp[i][j] =  result;
     }
     bool isInterleave(string s1, string s2, string s3) {
          n = s1.length();
          m = s2.length();
          N = s3.length();
-         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(N+1,-1)));
+         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         if(N > n+m || N < m+n)return false;
-        return f(0,0,0,s1,s2,s3,dp);
+        return f(0,0,s1,s2,s3,dp);
     }
 };
