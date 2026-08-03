c14f725056285297712571381827ddc49e8af6f6
pallavi188 <officialpallavi414@gmail.com>
2026-08-03 23:33:19 +0530

    Time: 4 ms (74.37%) | Memory: 19.2 MB (9.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen =0;
-        
-        for(int i=0;i<s.length();i++){
-            string t = "";
-            for(int j=i;j<s.length();j++){
-                if(t.find(s[j])==string::npos){
-                   t +=s[j];
-                   maxlen = max(maxlen,(int)t.length());
-                } else
-                break;
-                
+        int n = s.length();
+        vector<int>hash(256,-1);
+        int l = 0,r = 0,maxLen = 0;
+        while(r < n){
+            if(hash[s[r]] != -1){
+                if(hash[s[r]] >= l){
+                    l = hash[s[r]] + 1;
+                }
             }
+            int len = r-l+1;
+            maxLen = max(len,maxLen);
+            hash[s[r]] = r;
+            r++;
         }
-        return maxlen;
+        return maxLen;
     }
 };
