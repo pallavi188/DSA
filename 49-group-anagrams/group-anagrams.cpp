1ae4093dfe1ff263d1f26f950bc624fe23855441
pallavi188 <officialpallavi414@gmail.com>
2026-08-06 15:49:33 +0530

    Time: 15 ms (69.14%) | Memory: 25.9 MB (57.26%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    string generate(string &word){
+        int arr[26] = {0};
+        for(char &ch : word){
+            arr[ch-'a']++;
+        }
+        string new_word = "";
+        for(int i=0;i<26;i++){
+            int freq = arr[i];
+            if(freq>0){
+                new_word += string(freq,i+'a');
+            }
+        }
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        unordered_map<string,vector<string>>mp;
-        vector<vector<string>>ans;
-        for(int i=0;i<n;i++){
-            string temp = strs[i];
-            sort(begin(temp),end(temp));
-            mp[temp].push_back(strs[i]);
-        }
-        for(auto it : mp){
-            ans.push_back(it.second);
-        }
-        return ans;
+    int n = strs.size();
+    unordered_map<string,vector<string>>mpp;
+    vector<vector<string>>result;
+    for(int i=0;i<n;i++){
+        string word = strs[i];
+        string new_word = generate(word);
+        mpp[new_word].push_back(word);
+    }
+    for(auto &it:mpp){
+        result.push_back(it.second);
+    }
+    return result;
     }
 };
