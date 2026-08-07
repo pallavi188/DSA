f6eab1d2907fcf1a3ba8ea96633de0235c61a6ad
pallavi188 <officialpallavi414@gmail.com>
2026-08-07 15:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (94.39%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        int ans = 0;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-        }
-        for(auto &it:freq){
-            if(it.second == 1){
-                ans = it.first;
-                break;
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        int st = 0, end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (mid == 0 && nums[0] != nums[1])
+                return nums[0];
+            if (mid == n - 1 && nums[n - 1] != nums[n - 2])
+                return nums[n - 1];
+            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            } else {
+                if (mid % 2 == 0) {
+                    if (nums[mid - 1] == nums[mid]) {
+                        end = mid - 1;
+                    } else {
+                        st = mid + 1;
+                    }
+                } else {
+                    if (nums[mid - 1] == nums[mid]) {
+                        st = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
             }
         }
-        return ans;
+        return -1;
     }
 };
