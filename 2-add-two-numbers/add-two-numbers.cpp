c6437693433ccd3e3c8e15992ede71e7aca7d9ac
pallavi188 <officialpallavi414@gmail.com>
2026-08-11 15:41:26 +0530

    Time: 3 ms (34.19%) | Memory: 77.2 MB (46.78%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,27 +11,27 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
-        ListNode* dNode = new ListNode(-1);
-        ListNode* curr = dNode;
-        int carry = 0;
-        while(t1 != NULL || t2 != NULL){
-            int sum = carry;
-            if(t1) sum = sum + t1->val;
-            if(t2) sum = sum + t2->val;
-            ListNode* newNode = new ListNode(sum%10);
-            carry = sum/10;
-            curr->next = newNode;
-            curr = curr->next;
-            if(t1) t1 = t1->next;
-            if(t2) t2 = t2->next;
-
-        }
-            if(carry != 0){
-                ListNode* carryNode = new ListNode(carry);
-                curr->next = carryNode;
-            }
+       ListNode* t1 = l1;
+       ListNode* t2 = l2;
+       ListNode* dNode = new ListNode(-1);
+       ListNode* curr = dNode;
+       int carry = 0;
+       while(t1 != NULL || t2 != NULL){
+        int sum = carry;
+        if(t1) sum = sum + t1->val;
+        if(t2) sum = sum + t2->val;
+        ListNode* newNode = new ListNode(sum %10);
+        carry = sum/10;
+        curr->next = newNode;
+        curr = curr->next;
+        if(t1) t1 = t1->next;
+        if(t2) t2 = t2->next;
+       }
+       if(carry != 0){
+        ListNode* carryNode = new ListNode(carry);
+        curr->next = carryNode;
+       }
         return dNode->next;
     }
+   
 };
