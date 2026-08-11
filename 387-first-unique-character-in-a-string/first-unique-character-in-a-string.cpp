ded99eeff20be54748eee04fc1b7adbd9480608d
pallavi188 <officialpallavi414@gmail.com>
2026-08-11 19:49:26 +0530

    Time: 21 ms (17.20%) | Memory: 14.9 MB (39.65%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int>mp;
-        queue<int>q;
-        for(int i=0;i<s.length();i++){
-            if(mp.find(s[i]) == mp.end()) q.push(i);
-            mp[s[i]]++;
+       unordered_map<char,int>mp;
+       queue<int>q;
+       for(int i=0;i<s.length();i++){
+        if(mp.find(s[i]) == mp.end())q.push(i);
 
-            while(q.size()>0 && mp[s[q.front()]] > 1) q.pop();
-        }
-        if(q.empty())return -1;
-        else
-        return q.front();
+        mp[s[i]]++;
+       
+       while(q.size()>0 && mp[s[q.front()]]>1)q.pop();
+       }
+       if(q.empty())return -1;
+       else
+       return q.front();
     }
 };
