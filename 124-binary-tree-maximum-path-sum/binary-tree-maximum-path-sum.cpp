c4680db75433bab0ac4cf2086f556683c206acfc
pallavi188 <officialpallavi414@gmail.com>
2026-08-15 21:49:32 +0530

    Time: 3 ms (12.96%) | Memory: 27.9 MB (45.98%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -12,17 +12,16 @@
  */
 class Solution {
 public:
-    int maxpath(TreeNode* root, int &maxi) {
-        if (root == NULL)
-            return 0;
-        int leftSum = max(0, maxpath(root->left, maxi));
-        int rightSum = max(0, maxpath(root->right, maxi));
-        maxi = max(maxi, leftSum + rightSum + root->val);
-        return root->val + max(leftSum, rightSum);
+    int maxPath(TreeNode* root,int &maxVal){
+        if(!root) return 0;
+        int leftSum = max(0,maxPath(root->left,maxVal));
+        int rightSum = max(0,maxPath(root->right,maxVal));
+        maxVal = max(maxVal,leftSum + rightSum + root->val);
+        return root->val + max(leftSum , rightSum);
     }
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN;
-        maxpath(root, maxi);
-        return maxi;
+       int maxVal = -1e9;
+       maxPath(root,maxVal);
+       return maxVal;
     }
 };
