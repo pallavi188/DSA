09cd02e9a33c753981a92c90ecf6921d4b6df56e
pallavi188 <officialpallavi414@gmail.com>
2026-08-18 17:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (75.27%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int n,m;
-    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
-    void dfs(int row,int col,vector<vector<int>>&ans,vector<vector<int>>&image,int newColor,int initialColor){
-        ans[row][col] = newColor;
-        for(vector<int>&dir : directions){
-            int new_row = row + dir[0];
-            int new_col = col + dir[1];
-            if(new_row>=0 && new_row<n && new_col>=0&&new_col<m && ans[new_row][new_col] != newColor && image[new_row][new_col]==initialColor){
-                dfs(new_row,new_col,ans,image,newColor,initialColor);
+    void dfs(int sr,int sc,vector<vector<int>>&ans,vector<vector<int>>&image,
+    int color,int initialcolor){
+         ans[sr][sc] = color;
+         int n = image.size();
+         int m = image[0].size();
+         int dr[] = {-1,1,0,0};
+         int dc[] = {0,0,1,-1};
+         for(int i=0;i<4;i++){
+            int newRow = sr + dr[i];
+            int newCol = sc + dc[i];
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol < m && ans[newRow][newCol] != color && image[newRow][newCol] == initialcolor){
+                dfs(newRow,newCol,ans,image,color,initialcolor);
             }
-        }
+         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        n = image.size();
-        m = image[0].size();
+        int n = image.size();
+        int m = image[0].size();
        int initialColor = image[sr][sc];
        vector<vector<int>>ans = image;
        dfs(sr,sc,ans,image,color,initialColor);
