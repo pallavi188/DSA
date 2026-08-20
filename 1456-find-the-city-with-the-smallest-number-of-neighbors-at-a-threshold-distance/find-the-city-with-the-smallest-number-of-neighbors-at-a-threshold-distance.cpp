6c292287f70218ba7a173499019135f5f277e3e3
pallavi188 <officialpallavi414@gmail.com>
2026-08-20 16:36:18 +0530

    Time: 14 ms (61.81%) | Memory: 17.3 MB (29.68%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dist(n,vector<int>(n,1e9));
-        for(auto it : edges){
+         vector<vector<int>>dist(n,vector<int>(n,1e9));
+         for(auto it : edges){
             dist[it[0]][it[1]] = it[2];
             dist[it[1]][it[0]] = it[2];
-        }
-        for(int i=0;i<n;i++){
-            dist[i][i] =0;
-        }
-        for(int k =0;k<n;k++){
-            for(int i =0;i<n;i++){
+         }
+         for(int i=0;i<n;i++){
+            dist[i][i] = 0;
+         }
+         //floyd warsell algorithm
+         for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+                    dist[i][j] = min(dist[i][j] , dist[i][k]+dist[k][j]);
                 }
             }
-        }
-        int cntCity = n,cityNo = -1;
-        for(int city = 0;city<n;city++){
+         }
+         int cntCity = n,cityNo = -1;
+         for(int city=0;city<n;city++){
             int cnt = 0;
-            for(int adjCity = 0;adjCity<n;adjCity++){
-                if(dist[city][adjCity]<=distanceThreshold)cnt++;
+            for(int adjct = 0;adjct<n;adjct++){
+                if(dist[city][adjct] <= distanceThreshold) cnt++;
             }
             if(cnt <= cntCity){
                 cntCity = cnt;
                 cityNo = city;
             }
-        }
-        return cityNo;
+         }
+         return cityNo;
     }
 };
