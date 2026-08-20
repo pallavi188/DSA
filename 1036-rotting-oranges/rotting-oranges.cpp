869c4074ffa8ca6c779b212cd83ee3fc1a7c024b
pallavi188 <officialpallavi414@gmail.com>
2026-08-20 15:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (51.07%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-      int n = grid.size();
-      int m = grid[0].size();
-      //queue -> {row,col,time}
-      queue<pair<pair<int,int>,int>>q;
-     
-      //push those cell in the queue which contain  rotten oranges
-      for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j] == 2){
+         int n = grid.size();
+         int m = grid[0].size();
+         queue<pair<pair<int,int>,int>>q;
+        
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 2){
                 q.push({{i,j},0});
+              
+                }
             }
-        }
-      }
-      int time = 0;
-      int dr[] = {-1,1,0,0};
-      int dc[] = {0,0,-1,1};
-      while(!q.empty()){
-        int cur_row = q.front().first.first;
-        int cur_col = q.front().first.second;
-        int t = q.front().second;
-        q.pop();
-        time = max(time,t);
-        for(int i=0;i<4;i++){
-            int newRow = cur_row + dr[i];
-            int newCol = cur_col + dc[i];
-
-            //check the validity of each row 
-            if(newRow>=0 && newRow < n && newCol>=0 && newCol < m && grid[newRow][newCol] == 1){
-                q.push({{newRow,newCol},t+1});
-                grid[newRow][newCol] = 2;
+         }
+         int time = 0;
+         int dr[] = {-1,1,0,0};
+         int dc[] = {0,0,1,-1};
+         while(!q.empty()){
+            int curr_row = q.front().first.first;
+            int curr_col = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            time = max(time,t);
+            for(int i=0;i<4;i++){
+                int new_row = curr_row + dr[i];
+                int new_col = curr_col + dc[i];
+                if(new_row>=0 && new_row<n && new_col>=0 && new_col < m && 
+                   grid[new_row][new_col] == 1 ){
+                    q.push({{new_row,new_col},t+1});
+                    grid[new_row][new_col] = 2;
+                }
             }
-        }
-      }
-      //check if there is any cell which contain fresh orange or not
-      for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j] == 1)return -1;
-        }
-      }
-      return time;
+         }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1) return -1;
+            }
+         }
+         return time;
     }
 };
