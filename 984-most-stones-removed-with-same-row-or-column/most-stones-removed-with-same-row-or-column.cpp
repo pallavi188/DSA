f0c68628a1cf3af4af9223a63b2b5bcbe6c35277
pallavi188 <officialpallavi414@gmail.com>
2026-08-21 15:27:09 +0530

    Time: 78 ms (12.99%) | Memory: 17.9 MB (99.18%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void dfs(int idx,vector<vector<int>>& stones,vector<bool>&vis){
+    void dfs(int idx,vector<vector<int>>&stones,vector<bool>&vis){
+        int n = stones.size();
         vis[idx] = true;
-        for(int i=0;i<stones.size();i++){
-            if((!vis[i]) && (stones[i][0]==stones[idx][0] || stones[i][1] == stones[idx][1])){
+        for(int i=0;i<n;i++){
+            if((!vis[i]) && (stones[i][0] == stones[idx][0] || 
+               stones[i][1] == stones[idx][1])){
                 dfs(i,stones,vis);
-                vis[i] = true;
+               
             }
         }
     }
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
         vector<bool>vis(n,false);
-        int grp = 0;
+        int comp = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-               grp++;
-               dfs(i,stones,vis);
+                comp++;
+                dfs(i,stones,vis);
             }
         }
-        return (n-grp);
+        return (n-comp);
     }
 };
