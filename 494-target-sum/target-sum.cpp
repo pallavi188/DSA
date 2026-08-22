88a8f68459f2ed48ab4357e8911fd372bc028e89
pallavi188 <officialpallavi414@gmail.com>
2026-08-22 12:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (38.54%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int cntSubset(int idx,vector<int>&nums,int reqSum,vector<vector<int>>&dp){
-        int n = nums.size();
-        if(idx>=n)return reqSum == 0;
-        if(reqSum < 0)return 0;
-        if(dp[idx][reqSum] != -1) return dp[idx][reqSum];
-        int pick = cntSubset(idx+1,nums,reqSum - nums[idx],dp);
-        int not_pick = cntSubset(idx+1,nums,reqSum,dp);
-        return dp[idx][reqSum] = (pick + not_pick);
-    }
+   
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int totalSum = 0;
-       
         for(int i=0;i<n;i++)totalSum += nums[i];
         if(abs(target) > totalSum)return 0;
         if((totalSum+target)%2 != 0)return 0;
         int reqSum = (totalSum + target)/2;
-         vector<vector<int>>dp(n+1,vector<int>(reqSum+1,-1));
-        return cntSubset(0,nums,reqSum,dp);
+        vector<vector<int>>dp(n+1,vector<int>(reqSum+1,0));
+        //base case
+         dp[n][0] = 1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int rs = 0;rs<=reqSum;rs++){
+                int pick = 0;
+                if(nums[i] <= rs)
+                pick = dp[i+1][rs-nums[i]];
+                int not_pick = dp[i+1][rs];
+                dp[i][rs] = (pick + not_pick);
+            }
+        }
+        return dp[0][reqSum];
     }
 };
