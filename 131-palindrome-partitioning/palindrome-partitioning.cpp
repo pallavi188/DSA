ce181677fbf9404ff1b0bcb8933f3309fc50556b
pallavi188 <officialpallavi414@gmail.com>
2026-08-27 10:11:28 +0530

    Time: 31 ms (85.68%) | Memory: 53.4 MB (92.65%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPalin(string s){
-        int n = s.length();
-        int i=0,j=n-1;
+        int i =0;
+        int j = s.length()-1;
         while(i<=j){
             if(s[i] != s[j])return false;
             else{
@@ -12,24 +12,25 @@ public:
         }
         return true;
     }
-    void getAllParts(string s,vector<string>&partition,vector<vector<string>>&ans){
+    void getAllParts(string s,vector<string>&part,vector<vector<string>>&ans){
         if(s.length() == 0){
-            ans.push_back(partition);
+            ans.push_back(part);
             return;
         }
         for(int i=0;i<s.length();i++){
-            string part = s.substr(0,i+1);
-            if(isPalin(part)){
-                partition.push_back(part);
-                getAllParts(s.substr(i+1),partition,ans);
-                partition.pop_back();
+            string str = s.substr(0,i+1);
+            if(isPalin(str)){
+                part.push_back(str);
+                getAllParts(s.substr(i+1),part,ans);
+                 part.pop_back();
             }
+           
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>partition;
-        getAllParts(s,partition,ans);
-        return ans;
+       vector<vector<string>>ans;
+       vector<string>part;
+       getAllParts(s,part,ans);
+       return ans;
     }
 };
