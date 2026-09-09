28e706cc20a88ff3c954baadefb9b33d0ff0fdd3
pallavi188 <officialpallavi414@gmail.com>
2026-09-09 12:20:47 +0530

    Time: 1522 ms (29.42%) | Memory: 9.3 MB (77.41%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -14,8 +14,7 @@ public:
     }
     int f(int i,string &s,vector<int>&dp){
         int n = s.length();
-        if(i == n)return 0;
-        
+        if(i == n)return 0; 
         int minCost = INT_MAX;
         if(dp[i] != -1)return dp[i];
         for(int j=i;j<n;j++){
@@ -29,7 +28,17 @@ public:
     }
     int minCut(string s) {
         int n = s.length();
-        vector<int>dp(n,-1);
-        return f(0,s,dp)-1;
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int minCost = INT_MAX;
+            for(int j=i;j<n;j++){
+                if(isPalin(i,j,s)){
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost,cost);
+                }
+                dp[i] = minCost;
+            }
+        }
+        return dp[0]-1;
     }
 };
