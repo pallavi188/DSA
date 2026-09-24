862028281b0f486db04e17cdf6b5a7e9912ad389
pallavi188 <officialpallavi414@gmail.com>
2026-09-24 11:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (94.55%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -5,19 +5,18 @@ public:
         if(k < 0 || k>n)return -1;
         if(k == 0)return 0;
         if(k == n)return accumulate(card.begin(),card.end(),0);
-        int maxScore = -1e9;
-        vector<int>leftSum(k+1,0),rightSum(k+1,0);
-        //precomputing sum from left and right cards
-        for(int i=1;i<=k;i++){
-            leftSum[i] = leftSum[i-1] + card[i-1];
-            rightSum[i] = rightSum[i-1] + card[n-i];
-        }
-        //combine each valid left count and right count
-        for(int l=0;l<=k;l++){
-            int r = k-l;
-            int currScore = leftSum[l] + rightSum[r];
-            maxScore = max(maxScore,currScore);
+        int currScore = 0;
+        for(int i=0;i<k;i++){
+            currScore += card[i];
+        }  
+        int maxScore = currScore;
+        int rightIdx = n-1;
+        for(int i=k-1;i>=0;i--){
+            currScore -= card[i];
+            currScore += card[rightIdx];rightIdx--;
+            maxScore = max(maxScore , currScore);
         }
         return maxScore;
     }
+
 };
