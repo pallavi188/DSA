86b5b239721f6147c21ea828dadced7d9d478be1
pallavi188 <officialpallavi414@gmail.com>
2026-09-24 15:56:42 +0530

    Time: 31 ms (6.00%) | Memory: 72.8 MB (5.24%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-       int maxlen =0,l=0,r=0,zeros=0;
-       while(r<nums.size()){
-        if(nums[r]==0)zeros++;
-        if(zeros>k){
-            if(nums[l]==0)zeros--;
-            l++;
+        int n = nums.size();
+        vector<int>prefixZero(n+1,0);
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0)
+              prefixZero[i+1] = prefixZero[i] + 1;
+            else 
+              prefixZero[i+1] = prefixZero[i];  
         }
-        if(zeros<=k){
-            int len = r-l+1;
-            maxlen = max(len,maxlen);
+        int maxLen = 0;
+        //try every index as a starting point
+        for(int st=0;st<n;st++){
+            int l = st,h = n-1;
+            int farthest = st - 1;
+            //binary search to find the farthest zero
+            while(l <= h){
+                int mid = l + (h-l)/2;
+                int zeroCnt = prefixZero[mid+1] - prefixZero[st];
+                
+                if(zeroCnt <= k){
+                    farthest = mid;
+                    l = mid+1;
+                }
+                else{
+                    h = mid-1;
+                }
+            }
+            if(farthest >= st){
+                maxLen = max(maxLen,farthest-st+1);
+            }
         }
-        r++;
-       }
-       return maxlen;
+        return maxLen;
     }
 };
