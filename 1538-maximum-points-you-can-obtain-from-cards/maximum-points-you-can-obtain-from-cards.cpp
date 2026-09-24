46777fe791914828c5424718eb095eefd9ad4f74
pallavi188 <officialpallavi414@gmail.com>
2026-09-24 11:19:54 +0530

    Time: 3 ms (15.64%) | Memory: 48.4 MB (6.11%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int lsum =0,rsum =0,maxsum =0;
-      int rightIdx = cardPoints.size()-1;
-        for(int i=0;i<k;i++){
-            lsum += cardPoints[i];
-            maxsum = lsum;
+    int maxScore(vector<int>& card, int k) {
+        int n = card.size();
+        if(k < 0 || k>n)return -1;
+        if(k == 0)return 0;
+        if(k == n)return accumulate(card.begin(),card.end(),0);
+        int maxScore = -1e9;
+        vector<int>leftSum(k+1,0),rightSum(k+1,0);
+        //precomputing sum from left and right cards
+        for(int i=1;i<=k;i++){
+            leftSum[i] = leftSum[i-1] + card[i-1];
+            rightSum[i] = rightSum[i-1] + card[n-i];
         }
-        for(int j = k-1;j>=0;j--){
-            lsum -=cardPoints[j];
-            rsum += cardPoints[rightIdx];
-            rightIdx--;
-            maxsum = max(maxsum,lsum+rsum);
+        //combine each valid left count and right count
+        for(int l=0;l<=k;l++){
+            int r = k-l;
+            int currScore = leftSum[l] + rightSum[r];
+            maxScore = max(maxScore,currScore);
         }
-        return maxsum;
+        return maxScore;
     }
 };
