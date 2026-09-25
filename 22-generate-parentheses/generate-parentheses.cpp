4df2286c036858cc8bb96eae0aee6ab324196f92
pallavi188 <officialpallavi414@gmail.com>
2026-09-25 10:59:21 +0530

    Time: 3 ms (67.42%) | Memory: 13 MB (89.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void f(int n, int open, int close, string& current, vector<string>& ans) {
-        if (open == n && close == n) {
-            ans.push_back(current);
+    void f(int n,int open,int close,string &currStr,vector<string>&ans){
+        if(open == n && close == n){
+            ans.push_back(currStr);
             return;
         }
-        if (open < n) {
-            current.push_back('(');
-            f(n, open + 1, close, current, ans);
-            current.pop_back();
+        //ith char can be open bracket if no. of open bracket so far is less than n
+        if(open < n){
+            currStr.push_back('(');
+            f(n,open+1,close,currStr,ans);
+            currStr.pop_back();
         }
-        if (close < open) {
-            current.push_back(')');
-            f(n, open, close + 1, current, ans);
-            current.pop_back();
+        //ith char can be close bracket if no. of close bracket is less than the number of open bracket
+        if(close < open){
+            currStr.push_back(')');
+            f(n,open,close+1,currStr,ans);
+            currStr.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string current;
-         f(n, 0, 0, current, ans);
-         return ans;
+       vector<string>ans;
+       string currStr;
+       f(n,0,0,currStr,ans);
+       return ans;
     }
 };
