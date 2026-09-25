3f3deaedeef4fd5ad1cd24f38ec493f90994f06d
pallavi188 <officialpallavi414@gmail.com>
2026-09-25 12:01:12 +0530

    Time: 4 ms (35.98%) | Memory: 18.1 MB (20.92%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void f(int idx,vector<int>&list,vector<vector<int>>&ans,vector<int>&cand,int target){
-        if(idx==cand.size()){
-            if(target == 0){
-                ans.push_back(list);
+    void f(int idx,vector<int>&list,vector<vector<int>>&result,
+           vector<int>&cand,int target){
+        int n = cand.size();
+        if(idx == n){
+            if(target==0){
+                result.push_back(list);
             }
             return;
         }
-        //pick 
-        if(cand[idx] <= target){
+        //not pick
+         f(idx+1,list,result,cand,target);
+        //pick
+        if(cand[idx]<= target){
             list.push_back(cand[idx]);
-            f(idx,list,ans,cand,target-cand[idx]);
+            f(idx,list,result,cand,target-cand[idx]);
             list.pop_back();
         }
-        //not pick
-        f(idx+1,list,ans,cand,target);
     }
     vector<vector<int>> combinationSum(vector<int>& cand, int target) {
-       vector<int>list;
-       vector<vector<int>>ans;
-       f(0,list,ans,cand,target);
-       return ans;
+         vector<vector<int>>result;
+         vector<int>list;
+         f(0,list,result,cand,target);
+         return result;
     }
 };
